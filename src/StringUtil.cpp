1069a9089b91d969ce25a24fcbe1b6e171959637
Christopher Churas <churas@gmail.com>
2012-08-23 14:39:42 -0700

    added documentation

--- src/StringUtil.cpp
+++ src/StringUtil.cpp
@@ -14,14 +14,7 @@ StringUtil::StringUtil() {
 StringUtil::~StringUtil() {
 }
 
-/**
- * This method takes an integer and creates a string with padding of spaces
- * on left side of number so the string returned is of length size.
- * @param value Value to be written into string
- * @param maxvalue If value is greater then this value then ### will be printed
- * @param size width of resulting string
- * @return const char of string with spaces padding to left of value passed in
- */
+
 const char *StringUtil::getIntAsPaddedString(int value,int maxvalue,int width){
     
     if (width <= 0){
@@ -34,6 +27,7 @@ const char *StringUtil::getIntAsPaddedString(int value,int maxvalue,int width){
     }
     
       
+    //If value is greater then maxvalue just return a string with # characters in it
     if (value > maxvalue){
         return std::string(width,'#').c_str();
     }
--- src/StringUtil.hpp
+++ src/StringUtil.hpp
@@ -8,13 +8,40 @@
 #ifndef STRINGUTIL_HPP
 #define	STRINGUTIL_HPP
 
+/**
+ * Instances of this class provide a couple helper String methods
+ * @author churas
+ */
 class StringUtil {
     
+    /**
+     * Sets the max field width used by getIntAsPaddedString method
+     */
     static const int MAX_FIELD_SIZE_OF_INT = 10;
+    
 public:
+    
+    /**
+     * Constructor
+     */
     StringUtil();
+    
     virtual ~StringUtil();
-    const char *getIntAsPaddedString(int value,int maxvalue,int size);
+    
+    /**
+     * Given an integer (value) this method returns a string set to the width
+     * set by (width) method with the value left padded with spaces to reach
+     * the (width) specified.  If the width exceeds MAX_FIELD_SIZE_OF_INT constant
+     * which at the time of this writing is 10, then the width will be reset to 10.
+     * @param value Value to write into returned string.  If this value exceeds maxvalue 
+     *        then ### equal to width will be returned
+     * @param maxvalue any value above this value will cause this method to return ### 
+     *        as the return value
+     * @param width length in characters of returned string
+     * @return left padded string (padded by spaces) of value parameter or empty string if 
+     *         the width was less or equal to 0.
+     */
+    const char *getIntAsPaddedString(int value,int maxvalue,int width);
 private:
 
 };
