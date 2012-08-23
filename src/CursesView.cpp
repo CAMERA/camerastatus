1cdcb1a8fffea9fd9bf504f1e2be6ca1e403cf50
Christopher Churas <churas@gmail.com>
2012-08-23 15:39:11 -0700

    added documentation, also moved variables in constructor into more efficient initialization.  Moved width and height to constructor.  Added delete to all objects created by this object.  Added config.h include so that the title for the box gets the package version from config.h which comes from autoconf

--- src/CursesView.cpp
+++ src/CursesView.cpp
@@ -1,6 +1,6 @@
 
 #include "CursesView.hpp"
-
+#include "config.h"
 #include <iostream>
 
 #include <ncurses.h>
@@ -14,65 +14,96 @@
 #include "LoggedInUsersBar.hpp"
 #include "WorkflowsRunBar.hpp"
 
-CursesView::CursesView(StatusModel *sm){
-  // does not do anything yet
-    mp_pStatusModel = sm;
-    mp_pBox = NULL;
-    mp_pColors = NULL;
-    mp_pNews = NULL;
-    mp_pJobPanel = NULL;
-    mp_pClusterPanel = NULL;
-    mp_pLoggedInUsersBar = NULL;
-    mp_pWorkflowsRunBar = NULL;
+CursesView::CursesView(StatusModel *sm) : mp_pStatusModel(sm),
+mp_pBox(NULL),
+mp_pColors(NULL),
+mp_pNews(NULL),
+mp_pJobPanel(NULL),
+mp_pClusterPanel(NULL),
+mp_pLoggedInUsersBar(NULL),
+mp_pWorkflowsRunBar(NULL) {
+    mp_Width = 79;
+    mp_Height = 23;
+
 }
 
-CursesView::~CursesView(){
-  std::cout <<"CursesView destructor"<<std::endl;
-  endwin();
-  if (mp_pBox != NULL){
-    delete mp_pBox;
-  }
-  if (mp_pColors != NULL){
-    delete mp_pColors;
-  }
+CursesView::~CursesView() {
+    
+    //stop curses
+    endwin();
+    
+    
+    if (mp_pColors != NULL) {
+        delete mp_pColors;
+    }
+    if (mp_pNews != NULL){
+        delete mp_pNews;
+    }
+    
+    if (mp_pBox != NULL) {
+        delete mp_pBox;
+    }
+    if (mp_pJobPanel != NULL){
+        delete mp_pJobPanel;
+    }
+    if (mp_pClusterPanel != NULL){
+        delete mp_pClusterPanel;
+    }
+    if (mp_pLoggedInUsersBar != NULL){
+        delete mp_pLoggedInUsersBar;
+    }
+    if (mp_pWorkflowsRunBar != NULL){
+        delete mp_pWorkflowsRunBar;
+    }
+    
 }
 
-int CursesView::initialize(){
-  initscr();
-  if (has_colors() == FALSE){
-     return 1;
-  }
-  curs_set(0);
-  noecho();
-  cbreak();
-  
-  mp_pColors = new CursesColors();
-  mp_pColors->initialize(); 
-  mp_pNews = new News(1,1,4,79,mp_pColors,mp_pStatusModel);
-  mp_pBox = new Box(0,0,23,79,"Camera System Monitor v0.5",mp_pColors);
-  mp_pJobPanel = new JobPanel(1,6,3,79,mp_pColors,mp_pStatusModel);
-  mp_pClusterPanel = new ClusterPanel(1,10,6,79,mp_pColors,mp_pStatusModel);
-  mp_pLoggedInUsersBar = new LoggedInUsersBar(1,17,79,mp_pColors,mp_pStatusModel);
-  mp_pWorkflowsRunBar = new WorkflowsRunBar(1,18,79,mp_pColors,mp_pStatusModel);
-  return 0;
+int CursesView::initialize() {
+    
+    //initialize curses library
+    initscr();
+    
+    //verify we have colors with curses otherwise bail
+    if (has_colors() == FALSE) {
+        return 1;
+    }
+    //get rid of the cursor
+    curs_set(0);
+    
+    //don't echo any characters
+    noecho();
+    
+    //as soon as a user hits a key make it available to getch() 
+    cbreak();
+
+    mp_pColors = new CursesColors();
+    mp_pColors->initialize();
+    mp_pNews = new News(1, 1, 4, mp_Width, mp_pColors, mp_pStatusModel);
+    std::string title = std::string("Camera System Monitor v") + std::string(PACKAGE_VERSION);
+    mp_pBox = new Box(0, 0, mp_Height, mp_Width, title.c_str(), mp_pColors);
+    mp_pJobPanel = new JobPanel(1, 6, 3, mp_Width, mp_pColors, mp_pStatusModel);
+    mp_pClusterPanel = new ClusterPanel(1, 10, 6, mp_Width, mp_pColors, mp_pStatusModel);
+    mp_pLoggedInUsersBar = new LoggedInUsersBar(1, 17, mp_Width, mp_pColors, mp_pStatusModel);
+    mp_pWorkflowsRunBar = new WorkflowsRunBar(1, 18, mp_Width, mp_pColors, mp_pStatusModel);
+    return 0;
 }
 
-void CursesView::draw(){
-  mp_pBox->draw();
-  mp_pNews->draw();
-  mp_pJobPanel->draw();
-  mp_pClusterPanel->draw();
-  mp_pLoggedInUsersBar->draw();
-  mp_pWorkflowsRunBar->draw();
-  refresh();
+void CursesView::draw() {
+    mp_pBox->draw();
+    mp_pNews->draw();
+    mp_pJobPanel->draw();
+    mp_pClusterPanel->draw();
+    mp_pLoggedInUsersBar->draw();
+    mp_pWorkflowsRunBar->draw();
+    refresh();
 }
 
-void CursesView::update(){
-  mp_pBox->update();
-  mp_pNews->update();
-  mp_pJobPanel->update();
-  mp_pClusterPanel->update();
-  mp_pLoggedInUsersBar->update();
-  mp_pWorkflowsRunBar->update();
-  refresh();
+void CursesView::update() {
+    mp_pBox->update();
+    mp_pNews->update();
+    mp_pJobPanel->update();
+    mp_pClusterPanel->update();
+    mp_pLoggedInUsersBar->update();
+    mp_pWorkflowsRunBar->update();
+    refresh();
 }
--- src/CursesView.hpp
+++ src/CursesView.hpp
@@ -11,23 +11,120 @@ class LoggedInUsersBar;
 class WorkflowsRunBar;
 class StatusModel;
 
+/**
+ * Instances of this class represent the View of the application.  Instances of
+ * this object are responsibile for initialization of ncurses library as well
+ * as creation and updating of the various UI components of the view.  This
+ * view generates a UI of the following structure with labels denoting the
+ * various classes used:
+ * 
+ *          --Box------------------------
+ *          |    News                    |
+ *          |    JobPanel                |
+ *          |   ClusterPanel             |
+ *          |   LoggedInUsersBar         |
+ *          |   WorkflowsRunBar          |
+ *           ---------------------------- 
+ *
+ * Instances of this class first create the above objects and pass most if not
+ * all of them the StatusModel object passed into the constructor of this class.
+ * 
+ * When draw() method is invoked, this method calls draw() on all of the above
+ * objects.  When update() is called this method calls update() on all of the
+ * above objects.  
+ * The destructor deletes all objects the instance creates.  Before calling
+ * draw() or update() the initialize() method must be called.
+ */
 class CursesView {
+public:
 
-  public:
-    CursesView(StatusModel *sm);  //constructor  
+    /**
+     * Constructor
+     * @param sm Pointer to valid StatusModel object which is used to obtain data
+     *           to display
+     */
+    CursesView(StatusModel *sm); //constructor  
+
+    /**
+     * Destructor that deletes any objects instances of this class have created.
+     */
     ~CursesView(); //destructor
-    int initialize();  
-    void draw(); //
+
+    /**
+     * Initializes curses library.  Must be called before calling draw() or update()
+     * methods.  
+     * @todo should make the constructor invoke this method.
+     * @return 0 upon success or 1 if there was an error with initialization in
+     *         which case caller should abandon using this view.
+     */
+    int initialize();
+
+    /**
+     * Calls draw() on all UI objects which will render non changing UI portions
+     * of objects.
+     * @warning Must call initialize() before calling this method
+     */
+    void draw();
+
+    /**
+     * Calls update() on all UI objects which will render changing UI portions of
+     * objects.
+     * @warning Must call draw() once before calling this method.
+     */
     void update();
 
-  private:
+private:
+
+    /**
+     * Width of window UI.  This is passed to the UI components
+     */
+    int mp_Width;
+
+    /**
+     * Height of window UI.  This will be portioned out to some of the
+     * components
+     */
+    int mp_Height;
+
+    /**
+     * Box UI object
+     */
     Box *mp_pBox;
+
+    /**
+     * Allows UI components to set colors
+     */
     CursesColors *mp_pColors;
+
+    /**
+     * News section UI object
+     */
     News *mp_pNews;
+
+    /**
+     * Jobs section UI object
+     */
     JobPanel *mp_pJobPanel;
+
+    /**
+     * Cluster information section UI object
+     */
     ClusterPanel *mp_pClusterPanel;
+
+    /**
+     * Logged in users and last login section UI object
+     */
     LoggedInUsersBar *mp_pLoggedInUsersBar;
+
+    /**
+     * Number of workflows run UI object
+     */
     WorkflowsRunBar *mp_pWorkflowsRunBar;
+
+    /**
+     * Model used by the UI objects to obtain current information when update()
+     * is called on them
+     */
     StatusModel *mp_pStatusModel;
 };
 #endif //__CURSESVIEW_H_INCLUDED__
